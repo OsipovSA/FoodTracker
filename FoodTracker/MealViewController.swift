d74d1c5234e5aa228c3ed224c1a6af76c98d0e8a
Osipov <sergeyosipov-86@yandex.ru>
2021-03-16 18:16:20 +0700

    Прописал заполнение строки таблицы данными, переименовал ViewController в MealViewController. Теперь начальное представление будет являться лишь отображением данных строки таблицы

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController.swift
 //  FoodTracker
 //
 //  Created by Sergey on 15.03.2021.
@@ -7,12 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate,
+class MealViewController: UIViewController, UITextFieldDelegate,
     UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     //MARK: Properties
     
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
     
@@ -29,7 +28,7 @@ class ViewController: UIViewController, UITextFieldDelegate,
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        mealNameLabel.text = textField.text
+        
     }
     
     //MARK: Actions
