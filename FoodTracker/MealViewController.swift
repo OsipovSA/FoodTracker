0fc8fa6081b9618e3c8679c4a4f9ff8a19067ac3
Osipov <sergeyosipov-86@yandex.ru>
2021-03-16 20:00:24 +0700

    Сделал кнопку + на раскадровку списка, затем на раскадровку элемента сделал кнопки сохранить и отмена. Посмотрел как делается размотка перехода обратно (подготовка нужных данных для передачи в методе prepare в контроллере раскадровки элемента и затем получение этих данных в методе контроллера таблицы)

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import os.log
 
 class MealViewController: UIViewController, UITextFieldDelegate,
     UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -14,10 +15,14 @@ class MealViewController: UIViewController, UITextFieldDelegate,
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+
+    var meal: Meal?
     
     override func viewDidLoad() {
-        nameTextField.delegate = self
         super.viewDidLoad()
+        nameTextField.delegate = self
+        updateSaveButtonState()
     }
     
     //MARK: UITextFieldDelegate
@@ -27,8 +32,13 @@ class MealViewController: UIViewController, UITextFieldDelegate,
         return true
     }
     
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        saveButton.isEnabled = false
+    }
+    
     func textFieldDidEndEditing(_ textField: UITextField) {
-        
+        updateSaveButtonState()
+        navigationItem.title = textField.text
     }
     
     //MARK: Actions
@@ -38,6 +48,23 @@ class MealViewController: UIViewController, UITextFieldDelegate,
         imagePickerController.sourceType = .photoLibrary
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    //MARK: Navigation
+    @IBAction func Cancel(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+            os_log("The save button was not pressed, cancelling",log: OSLog.default, type: .debug)
+            return
+        }
+        let name = nameTextField.text ?? ""
+        let photo = photoImageView.image
+        let rating = ratingControl.rating
+        meal = Meal(name: name, photo: photo, rating: rating)
     }
     
     //MARK: UIImagePickerControllerDelegate
@@ -52,6 +79,12 @@ class MealViewController: UIViewController, UITextFieldDelegate,
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: Private Methods
+    private func updateSaveButtonState(){
+        let text = nameTextField.text ?? ""
+        saveButton.isEnabled = !text.isEmpty
     }
 }
 
